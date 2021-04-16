5268d6ff7d69b3ffcb0751284b5790ee6e8c254c
looyea <ly@Git123>
2021-04-16 14:17:59 +0800

    清理了目录内容和结构

--- __main__.py
+++ __main__.py
@@ -6,11 +6,6 @@ Created on Sun Oct  4 14:45:35 2020
 """
 
 from basecfg import taskChains
-import pandas as pd
-import global_static as gs
-import global_static_range as gsr
-import global_draw as gd
-import global_draw_range as gdr
 
 for taskChain in taskChains:
     taskChain.init()
