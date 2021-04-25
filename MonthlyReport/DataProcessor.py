9e61c6d9eaebe2e7c25e90937064f0d9bbbb32bb
looyea <ly@Git123>
2021-04-25 13:57:59 +0800

    完善了对数据的基础处理修正

--- MonthlyReport/DataProcessor.py
+++ MonthlyReport/DataProcessor.py
@@ -7,19 +7,38 @@ class DataProcessor:
     def __init__(self, ctx=None):
         self.__ctx__ = ctx
 
-    def __dataWashing(self, data):
-        return data
+    def __dataWashing(self):
+        l1_evt_idx = self.__ctx__['l1_evt_idx'].split(",")
+        l2_evt_idx = self.__ctx__['l2_evt_idx'].split(",")
+        len1 = len(l1_evt_idx)
+        len2 = len(l2_evt_idx)
 
-    def __dataTransform(self, data):
+        df1 = pd.DataFrame(data=np.zeros((len1,), dtype=int), index=l1_evt_idx, columns=['历时分钟1'])
+        df2 = pd.DataFrame(data=np.zeros((len2,), dtype=int), index=l2_evt_idx, columns=['历时分钟1'])
+        df1.index.name = '事件'
+        df2.index.name = '事件'
 
+        self.__ctx__['l1_evt_base'] = df1
+        self.__ctx__['l2_evt_base'] = df2
+
+    def __merge(self, base, data):
+
+        base = pd.concat([base, data], axis=1, join='outer')
+        base = base.drop(columns=['历时分钟1'])
+        base = base.fillna(0)
+        return base
+
+
+    def __dataTransform(self):
         results = dict()
+        self.__ctx__['results'] = results
+        data = self.__ctx__['data']
 
         l1pt = pd.pivot_table(
             data[self.__ctx__['cur_month']],
             values='历时分钟',
             index='时间分类',
             aggfunc=np.sum)
-        print(l1pt)
 
         eventpt = pd.pivot_table(
             data[self.__ctx__['cur_month']],
@@ -27,31 +46,30 @@ class DataProcessor:
             index=['时间分类', '事件'],
             aggfunc=np.sum
         )
-        print(eventpt)
         l1event = eventpt.loc['I类时间']
         l2event = eventpt.loc['II类时间']
-        print(l1event)
-        print(l2event)
+        l1event = self.__merge(self.__ctx__['l1_evt_base'], l1event)
+        l2event = self.__merge(self.__ctx__['l2_evt_base'], l2event)
+        print(l1event, l2event)
+        del (eventpt)
 
         results['l1pt'] = l1pt
         results['l1event'] = l1event
         results['l2event'] = l2event
-        return results
 
-    def __dataAdapting(self, data):
-        return data
+
+    def __dataAdapting(self):
+        pass
 
     def process(self):
-        data = self.__ctx__['data']
+
         # piovt清洗
-        results = self.__dataWashing(data)
+        self.__dataWashing()
 
         # pivot变换
-        results = self.__dataTransform(results)
+        self.__dataTransform()
 
         # 适配数据输出
-        results = self.__dataAdapting(results)
+        self.__dataAdapting()
 
         print("数据处理完毕！")
-
-        self.__ctx__['results'] = results
